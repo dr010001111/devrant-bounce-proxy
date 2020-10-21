98166dd260031cf3efc877ac72b3f6175e62ca76
dr010001111 <dr010001111@users.noreply.github.com>
2020-10-21 19:03:41 +0200

    working alpha

--- server/server.ts
+++ server/server.ts
@@ -5,53 +5,71 @@ import bodyParser = require('body-parser');
 import { v4 as uuidv4 } from 'uuid';
 
 // @ts-ignore
-import proxy = require('express-http-proxy');
-
-// @ts-ignore
 import fetch = require('node-fetch');
 
-const { spawn } = require('child_process');
+const httpProxy = require('http-proxy');
 
 // const motherland = "https://proxy.devrant.app";
-const motherland = "http://localhost:6969";
-
-const randomPort = Math.round(Math.random() * 1e4);
-const proxyPort = randomPort + 1;
-
-const localCorsProxy = `http://localhost:${proxyPort}/proxy/`
+const masterPort = 4421;
+const motherland = `http://localhost:${masterPort}`;
 
 const isMaster = process.argv[process.argv.length - 1].match("--master")
+const usePort = isMaster ? masterPort : 1024 + Math.round(Math.random() * 1e3);
 
 const app: express.Application = express();
 app.use(bodyParser.json());
 
 const thisServerUUID = uuidv4();
 
+var proxyServer = httpProxy.createProxyServer({});
+
 interface CountersDef {
-  [serverId: string]: number
+  [serverId: string]: {
+    target: string;
+    count: number;
+  }
 }
 
-const counters: CountersDef = {}
+let registry: CountersDef = {
+  [thisServerUUID]: {
+    target: 'https://devrant.com',
+    count: 0
+  }
+}
 
-function register() {
-  fetch(motherland + '/register', {
+async function sync() {
+  const response = await fetch(motherland + '/sync', {
     method: "POST",
     headers: {
       "content-type": "application/json"
     },
     body: JSON.stringify({
-      uuid: thisServerUUID
+      uuid: thisServerUUID,
+      port: usePort
     })
   })
+
+  if (response.ok) {
+    registry = await response.json()
+  } else {
+    console.error('Unable to sync!!!', response)
+  }
 }
 
 function main() {
-  app.post('/register', function (req, res) {
+  app.post('/sync', function (req, res) {
     if (req.body && req.body.uuid) {
-      const id = req.body.uuid
-      counters[id] = 0;
-      res.json(counters);
-      console.log(`REGISTER ${id}`);
+      const { port, uuid: id } = req.body
+      var ip = String(req.headers['x-forwarded-for'] || req.connection.remoteAddress);
+      if (ip.substr(0, 7) == "::ffff:") {
+        ip = ip.substr(7)
+      }
+      res.json(registry);
+      registry[id] = {
+        count: 0,
+        target: `http://${ip}:${port}`
+      };
+      console.log(`Register ${ip} ${id}`);
     } else {
       res.json({
         error: "No server UUID given"
@@ -59,24 +77,34 @@ function main() {
     }
   });
 
-  app.all('*', proxy(localCorsProxy, {
-    proxyReqPathResolver: (req: any, res: any) => {
-      return req.url.replace(/^(\/proxy\/|\/)/gi, '/proxy/')
-    }
-  }));
+  // app.all('*', proxy(localCorsProxy, {
+  //   proxyReqPathResolver: (req: any, res: any) => {
+  //     return req.url.replace(/^(\/proxy\/|\/)/gi, '/proxy/')
+  //   }
+  // }));
 
-  const child = spawn('npx', ['local-cors-proxy', "--proxyUrl", "https://www.devrant.io", "--port", proxyPort]);
-
-  child.stdout.pipe(process.stdout)
-  child.stderr.pipe(process.stderr);
-
-  child.on('close', (code: any) => {
-    console.log(`child process exited with code ${code}`);
+  app.all('*', (req, res, next) => {
+    const prioritized = Object.entries(registry).sort(
+      ([_aId, a], [_bId, b]) => a.count > b.count ? 0 : -1
+    )
+    const [_id, proxy] = prioritized[0];
+    const target = proxy.target;
+    res.header('X-Proxied-By', "devrant.app Proxy Service");
+    proxyServer.web(req, res, { target, changeOrigin: 'http://devrant.com/' }, next);
+    res.header('Location', '')
+    registry[_id].count = registry[_id].count + 1
+    process.stdout.write(`>> ${target}${req.path} proxy to lowest target ${proxy.count} with id ${_id} at ${proxy.target}\r`);
   });
 
-  app.listen(isMaster ? 6969 : randomPort, function () {
-    console.log('App is listening!', !isMaster && randomPort);
-    register()
+  proxyServer.on('proxyRes', function (proxyRes: any, req: any, res: any) {
+    delete proxyRes.headers['location'];
+  });
+
+  app.listen(usePort, function () {
+    console.log('Proxy is listening!', usePort);
+    if (!isMaster) {
+      sync()
+    }
   });
 }
 
